5a43ced098429ca5a49d6de2350a135433af4e90
Stacey Brosnan <staceybrosnan@gmail.com>
2020-09-30 15:39:04 +1000

    gameIcons

--- js/game.js
+++ js/game.js
@@ -1,9 +1,14 @@
+$(".XImage").hide();
+$(".OImage").hide();
 
 let gameOver = false;
+let scoreO = 0;
+let scoreX = 0;
 let gameCount = 0;
 
 let turn = 1;
 
+//Player can choose if they if they would like to be X or O
 const player = function (player){
 
   player =  prompt("Are you x or o?");
@@ -15,39 +20,83 @@ const player = function (player){
 };
 };
 
+// const gameFinish = function(){
+//   if (score)
+//
+//
+// };
+
+//calculates the score
+const scoreBoard = function(player){
+
+    if ( player === "O" ){
+      scoreO = scoreO + 1;
+      $(`#O`).text(`${scoreO}`);
+      $(".block").removeClass("X");
+    }
+      else if ( player === "X" ){
+      scoreX = scoreX + 1;
+      $(`#X`).text(`${scoreX}`);
+      $(".block").removeClass("O");
+    }
+};
+
+
 $(document).ready(function(){
 
-  $('#start').on('click', function(){
+  $('#newGame').on('click', function(){
+    $(".block").css({backgroundColor: "red"});
+    $(".block").removeClass("X");
+    $(".block").removeClass("O");
+  });
+
+    $('#reset').on('click', function(){
+    $(".block").css({backgroundColor: "red"});
+    $("#X").text("0");
+    $("#O").text("0");
     player();
   });
 
+  $('#start').on('click', function(){
+    $(".block").css({backgroundColor: "red"});
+    $("#X").text("0");
+    $("#O").text("0");
+    player();
+  });
 
+//Goes between Players and adds a class when the player clicks a space
   $(".block").click(function() {
     if (turn === 1) {
       console.log("Player O");
       turn = 2;
+      $(this).find(".OImage").show();
       $(this).addClass("O");
       winCheck("O", "O");
+
     }
     else {
       console.log("Player X");
+      $(this).find(".XImage").show();
       $(this).addClass("X");
+      // $("#X").show();
       turn = 1;
       winCheck("X", "X");
       }
 
 });
 
-const winCheck = function(symbol, win) {
+
+//conditions to check if any player has won
+const winCheck = function(symbol, player) {
 
       if  ($("#0").hasClass(symbol) &&
           $("#1").hasClass(symbol) &&
           $("#2").hasClass(symbol))
         {
-            $("#0").css({backgroundColor: "green"});
-            $("#1").css({backgroundColor: "green"});
-            $("#2").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            $("#0").css({backgroundColor: "#daffe7"});
+            $("#1").css({backgroundColor: "#daffe7"});
+            $("#2").css({backgroundColor: "#daffe7"});
+            scoreBoard(player);
             return true;
         }
         else if ($("#3").hasClass(symbol)
@@ -57,7 +106,7 @@ const winCheck = function(symbol, win) {
             $("#3").css({backgroundColor: "green"});
             $("#4").css({backgroundColor: "green"});
             $("#5").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            scoreBoard(player);
             return true;
 
           } else if ($("#6").hasClass(symbol)
@@ -67,7 +116,7 @@ const winCheck = function(symbol, win) {
             $("#6").css({backgroundColor: "green"});
             $("#7").css({backgroundColor: "green"});
             $("#8").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            scoreBoard(player);
             return true;
 
         } else if ($("#0").hasClass(symbol)
@@ -77,7 +126,7 @@ const winCheck = function(symbol, win) {
             $("#0").css({backgroundColor: "green"});
             $("#3").css({backgroundColor: "green"});
             $("#6").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            scoreBoard(player);
             return true;
 
         } else if ($("#1").hasClass(symbol)
@@ -87,7 +136,7 @@ const winCheck = function(symbol, win) {
             $("#1").css({backgroundColor: "green"});
             $("#4").css({backgroundColor: "green"});
             $("#7").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            scoreBoard(player);
             return true;
         }
         else if ($("#2").hasClass(symbol)
@@ -97,7 +146,7 @@ const winCheck = function(symbol, win) {
             $("#2").css({backgroundColor: "green"});
             $("#5").css({backgroundColor: "green"});
             $("#8").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            scoreBoard(player);
             return true;
         }
         else if ($("#0").hasClass(symbol)
@@ -107,7 +156,7 @@ const winCheck = function(symbol, win) {
             $("#0").css({backgroundColor: "green"});
             $("#4").css({backgroundColor: "green"});
             $("#8").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            scoreBoard(player);
             return true;
         }
         else if ($("#2").hasClass(symbol)
@@ -117,7 +166,7 @@ const winCheck = function(symbol, win) {
             $("#2").css({backgroundColor: "green"});
             $("#4").css({backgroundColor: "green"});
             $("#6").css({backgroundColor: "green"});
-            gameCount = gameCount + 1;
+            scoreBoard(player);
             return true;
         } else {
             return false;
---
+++ readMe.md
@@ -0,0 +1,24 @@
+# Tic-Tac-Toe
+
+## Hyperlinks work in mark down
+[Here is the link to W3C](http//w3c.org)
+
+## Can also do images
+![Kitten picture](http://placekitten.com/120/120)
+
+## Code formatting
+In JS we often use the `document` variable.
+
+
+```const hey = function() {
+    console.log('wowoowo');
+}
+```
+
+## Blockquotes
+
+> ham is to hamster as Java is to script
+
+
+
+list bugs
